3a24ee582f6abdb3cf57070b4a00748db606ab05
Manisha L <manisha.l@ibm.com>
2023-04-14 01:36:39 +0530

    Commiting UPI and COD

---
+++ COD.java
@@ -0,0 +1,5 @@
+public class COD{
+	public static void main(String args[]){
+		System.out.println("COD");
+	}
+}
---
+++ UPI.java
@@ -0,0 +1,5 @@
+public class UPI{
+	public static void main(String args[]){
+		System.out.println("UPI");
+	}
+}
