61cacf6e616841e2738e9b038ef14f56e7a9c017
Manisha L <manisha.l@ibm.com>
2023-04-14 01:34:11 +0530

    Commiting Clothing and Gadgets

---
+++ Clothing.java
@@ -0,0 +1,5 @@
+public class Clothing{
+	public static void main(String args[]){
+		System.out.println("Clothing");
+	}
+}
---
+++ Gadgets.java
@@ -0,0 +1,5 @@
+public class Gadgets{
+	public static void main(String args[]){
+		System.out.println("Gadgets");
+	}
+}
